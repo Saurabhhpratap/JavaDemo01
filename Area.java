e9b33b4a4352e9f5b592340aedc8331861c84e2b
Saurabh Singh <saurabhhpratap@gmail.com>
2023-04-11 17:16:00 +0530

    This program calculates Area of Circle

---
+++ Area.java
@@ -0,0 +1,12 @@
+
+public class Area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float radius = 9.6f;
+final float pie = 3.14f;
+double area = pie * radius * radius ;
+System.out.println("Area of circle :" + String.format("%.2f",area));
+	}
+
+}
