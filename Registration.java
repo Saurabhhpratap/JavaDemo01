82303c0b555d05526f3494e0921af19332e72ec5
Saurabh Singh <saurabhhpratap@gmail.com>
2023-04-11 17:36:00 +0530

    Demo on Registration

---
+++ Registration.java
@@ -0,0 +1,22 @@
+
+public class Registration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String name = "Saurabh Singh";
+String college = "GLA";
+String emailID = "saurabhhpratap@gmail.com";
+Long phone_number = 9068001522L;
+String branch = "CSE";
+Short Year_of_passout = 2018;
+boolean has_backlog = false;
+System.out.println("Name of student: " + name);
+System.out.println("College Name: " + college);
+System.out.println("Email ID: " + emailID);
+System.out.println("Phone Number: " + phone_number);
+System.out.println("Branch: " + branch);
+System.out.println("Year of Pass Out: " + Year_of_passout);
+System.out.println("Has Backlog: " + has_backlog);
+	}
+
+}
