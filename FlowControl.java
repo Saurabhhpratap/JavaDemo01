12b22cbd620f774a9aee954e2898ff5c557df629
Saurabh Singh <saurabhhpratap@gmail.com>
2023-04-11 17:33:04 +0530

    Demo on FlowControl

---
+++ FlowControl.java
@@ -0,0 +1,25 @@
+
+public class FlowControl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+int a =140,b=75,c=150;
+
+if(a>=b && a>=c)
+{
+	System.out.println("Largest number of 3 is  :" +a);
+}
+
+if(b>=a && b>=c)
+{
+	System.out.println("Largest number of 3 is  :" +b);
+}
+
+if(c>=a && c>=b)
+{
+	System.out.println("Largest number of 3 is  :" +c);	
+}
+	}
+}
